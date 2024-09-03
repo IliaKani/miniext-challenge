a7e69162de85043740730e79c7192426224e76f0
Ilia Kani <kanishchev.ilia@gmail.com>
2024-09-03 12:58:58 +0200

    changed firebase config

--- components/firebase/firebase.ts
+++ components/firebase/firebase.ts
@@ -1,14 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { /* connectFirestoreEmulator, */ getFirestore } from 'firebase/firestore';
-import { /* connectStorageEmulator, */ getStorage } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 import 'firebase/auth';
-// import { isDev } from '../isDev';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -18,16 +12,8 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
 // Initialize Firebase
-export const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
 
 export const firestore = getFirestore(firebaseApp);
 export const baseBucketName = 'sign-in-form-d02de.appspot.com';
-
-/* if (isDev) {
-    connectFirestoreEmulator(firestore, '127.0.0.1', 8081);
-} */
